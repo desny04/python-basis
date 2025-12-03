13bf31fec5c8a14c07a542d3b7e9b6106a665f2f
desny <trsadesny@gmail.com>
2025-12-03 11:21:48 +0530

    python basis

---
+++ loop.py
@@ -0,0 +1,77 @@
+#for loop
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# for i in range(1,11):
+#     if i%2==0:
+#         print(i)
+
+
+# a=int(input("Enter First num:"))
+# b=int(input("Enter Second num:"))
+
+# for i in range(a,b):
+#     print(i)
+
+# sum=0
+# for i in range(1,11):
+#     sum+=i
+# print(sum)
+
+
+#while loop
+
+# i=1
+# while(i<=10):
+#     print(i)
+#     i+=1
+
+# i=10
+# while(i>=1):
+#     print(i)
+#     i-=1
+
+# i=0
+# while(i<=10):
+#     i+=1
+#     if i%2==0:
+#         print(i)
+
+
+# a=int(input("Enter 1st num:"))
+# b=int(input("Enter 2nd num:"))
+
+# while(a<=b):
+#     print(a)
+#     a+=1
+
+# sum=0
+# i=1
+# while(i<=10):
+#     i+=1
+#     sum=sum+i
+# print(sum)
+
+# i=1
+# while(i<=5):
+
+#     print(i)
+#     i+=1
+
+
+#horizontal print
+
+# for i in range(1,11):
+#     print(i,end=' ')
+
+
+#nested loop
+
+for i in range(4):
+    for j in range(4):
+        print('*', end=' ')
+    print()
